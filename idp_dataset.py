95a8b5f7664770fc5dbdac1eef1d7221b3043f4a
austinv11 <austinv11@gmail.com>
2022-04-12 13:29:35 -0400

    Fix handling of the sequence encoding

--- idp_dataset.py
+++ idp_dataset.py
@@ -33,7 +33,9 @@ class IdpDataset(Dataset):
         for file in tqdm(os.listdir(self.directory), desc="Loading data from {}".format(self.directory)):
             if not file.endswith('.parquet'):
                 continue
-            self.proteins.append(pd.read_parquet(osp.join(self.directory, file)))
+            df = pd.read_parquet(osp.join(self.directory, file))
+            df['sequence'] = df['sequence'].apply(AAs.index)
+            self.proteins.append(df)
         self.label_names = ['is_disordered', 'is_binding', 'is_linker', 'is_protein_binding', 'is_nucleic_acid_binding']
         self.feat_names = self.proteins[0].loc[:, ~self.proteins[0].columns.isin(self.label_names)].columns.values.tolist()
 
@@ -42,8 +44,12 @@ class IdpDataset(Dataset):
 
     def __getitem__(self, idx):
         df = self.proteins[idx]
-        df['sequence'] = df['sequence'].apply(AAs.index)
 
         labels = torch.from_numpy(df.loc[:, self.label_names].values)
         feats = torch.from_numpy(df.loc[:, self.feat_names].values)
         return feats, labels
+
+
+if __name__ == '__main__':
+    dataset = IdpDataset(DatasetMode.TRAIN)
+    print(dataset[0])
